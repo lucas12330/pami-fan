35070fd2b69d442ab7ed5a631d20a62a648e8ff3
lucas12330 <ligueketoniejudo@gmail.com>
2025-02-13 20:54:33 +0100

    Ajout d'une fonction de gestion de moteur et ajout d'une nouvelle action

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
-debut = 0
-boolBegin = False
 timeStart = 0
+boolBegin = False
+debut2 = 0
+debut = 0
+
+#Permet de géré les moteur plus facilement entre -255 et 255
+def Motors(Mx1, Mx2):
+    #géré le dépassement de la vitesse physique maximale
+    if -255 < Mx1 < 255 and -255 < Mx2 < 255:
+        if Mx1 > 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, Mx1)
+        else:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, -(Mx1))
+        
+        if Mx2 > 0:
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, Mx2)
+        else:
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, -(Mx2))
+    else:
+        if Mx1 > 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        else:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, -(255))
+                
+        if Mx2 > 0:
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        else:
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, -(255))
+
 
 def on_forever():
-    global timeStart, boolBegin
-
-    debut = pins.digital_read_pin(DigitalPin.P0)
-    if debut != 0 and not boolBegin:
+    global debut2, timeStart, boolBegin
+    debut2 = pins.digital_read_pin(DigitalPin.P0)
+    if input.button_is_pressed(Button.A) or debut2 != 0 and not (boolBegin):
         timeStart = control.millis()
         boolBegin = True
         basic.show_leds("""
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        """)
-
-    if boolBegin:
-        elapsedTime = control.millis() - timeStart
-        if 85000 <= elapsedTime <= 100000:
-            if maqueen.ultrasonic() >= 5:  # Continue à avancer si pas d'obstacle
-                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 200)
-                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 200)
-            else:  # Arrête le robot s'il y a un obstacle
-                maqueen.motor_stop(maqueen.Motors.ALL)
-        elif elapsedTime > 100000:  # Arrêt définitif après 100 secondes
-            maqueen.motor_stop(maqueen.Motors.ALL)
-            boolBegin = False
-            basic.show_leds("""
             . . . . .
-            . . . . .
+            . . # . .
             . # # # .
-            . . . . .
+            . . # . .
             . . . . .
             """)
-
+    if boolBegin:
+        elapsedTime = control.millis() - timeStart
+        if 10000 <= elapsedTime and elapsedTime <= 20000:
+            if maqueen.ultrasonic() >= 5:
+                # Continue à avancer si pas d'obstacle
+                maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 200)
+                maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 200)
+            else:
+                # Arrête le robot s'il y a un obstacle
+                maqueen.motor_stop(maqueen.Motors.ALL)
+        elif 20000 > elapsedTime and elapsedTime > 30000:
+            # Arrêt définitif après 100 secondes
+            maqueen.motor_stop(maqueen.Motors.ALL)
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 200)
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+            basic.show_leds("""
+                . . . . .
+                . . . . .
+                . # # # .
+                . . . . .
+                . . . . .
+                """)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,45 +1,80 @@
-let debut = 0
-let boolBegin = false
 let timeStart = 0
+let boolBegin = false
+let debut2 = 0
+let debut = 0
+// Permet de géré les moteur plus facilement entre -255 et 255
+function Motors(Mx1: number, Mx2: number) {
+    // géré le dépassement de la vitesse physique maximale
+    if (-255 < Mx1 && Mx1 < 255 && (-255 < Mx2 && Mx2 < 255)) {
+        if (Mx1 > 0) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, Mx1)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, -Mx1)
+        }
+        
+        if (Mx2 > 0) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, Mx2)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, -Mx2)
+        }
+        
+    } else {
+        if (Mx1 > 0) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, -255)
+        }
+        
+        if (Mx2 > 0) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, -255)
+        }
+        
+    }
+    
+}
+
 basic.forever(function on_forever() {
     let elapsedTime: number;
     
-    let debut = pins.digitalReadPin(DigitalPin.P0)
-    if (debut != 0 && !boolBegin) {
+    debut2 = pins.digitalReadPin(DigitalPin.P0)
+    if (input.buttonIsPressed(Button.A) || debut2 != 0 && !boolBegin) {
         timeStart = control.millis()
         boolBegin = true
         basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
     }
     
     if (boolBegin) {
         elapsedTime = control.millis() - timeStart
-        if (85000 <= elapsedTime && elapsedTime <= 100000) {
+        if (10000 <= elapsedTime && elapsedTime <= 20000) {
             if (maqueen.Ultrasonic() >= 5) {
                 //  Continue à avancer si pas d'obstacle
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 200)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 200)
             } else {
                 //  Arrête le robot s'il y a un obstacle
                 maqueen.motorStop(maqueen.Motors.All)
             }
             
-        } else if (elapsedTime > 100000) {
+        } else if (20000 > elapsedTime && elapsedTime > 30000) {
             //  Arrêt définitif après 100 secondes
             maqueen.motorStop(maqueen.Motors.All)
-            boolBegin = false
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 200)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
             basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # .
-            . . . . .
-            . . . . .
-            `)
+                . . . . .
+                . . . . .
+                . # # # .
+                . . . . .
+                . . . . .
+                `)
         }
         
     }
